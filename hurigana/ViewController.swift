b81a6a17f57a0f2b01777dbefa3c193f3b023072
Tetsuya sano <jjjjjjkhaka@gmail.com>
2020-04-12 21:53:42 +0900

    first commit

---
+++ hurigana/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hurigana
+//
+//  Created by 佐野哲也 on 2020/04/12.
+//  Copyright © 2020 佐野哲也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
