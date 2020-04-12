eb5a61740fb5848dffdd2c63ba9ddb2e5ca68db8
Tetsuya sano <jjjjjjkhaka@gmail.com>
2020-04-12 22:43:34 +0900

    ひらがなAPIの導入
    入力された漢字まじりのテキストをgooの振り仮名APIに入力するシステムを作成した。
    入力されたものをWebAPIに流して、変換する。
    その結果をアプリに表示する。

--- hurigana/ViewController.swift
+++ hurigana/ViewController.swift
@@ -8,7 +8,17 @@
 
 import UIKit
 
-
+struct Rubi:Codable {
+    var request_id: String
+    var output_type: String
+    var converted: String
+}
+struct PostData: Codable {
+    var app_id:String
+    var request_id: String
+    var sentence: String
+    var output_type: String
+}
 class ViewController: UIViewController {
 
     @IBOutlet var textField: UITextField!
@@ -23,8 +33,45 @@ class ViewController: UIViewController {
     
     @IBAction func test(_ sender: Any) {
         inputText = textField.text!
-        outputText.text = inputText
-        print(inputText)
+        //outputText.text = inputText
+        
+        // URLRequstの設定
+        var request = URLRequest(url: URL(string: "https://labs.goo.ne.jp/api/hiragana")!)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        //POSTするデータをURLRequestに持たせる
+        let postData = PostData(app_id: "8b90aa7f78d3bceb571631b29757833963a5e82b8df687f35eca4423bf2d7e09", request_id: "record003", sentence: inputText, output_type: "hiragana")
+        guard let uploadData = try? JSONEncoder().encode(postData) else {
+            print("json生成に失敗しました")
+            return
+        }
+            request.httpBody = uploadData
+            //APIへPOSTしてresponseを受け取る
+            let task = URLSession.shared.uploadTask(with: request, from: uploadData) {
+                data, response, error in
+                if let error = error {
+                    print ("error: \(error)")
+                    return
+                }
+                guard let response = response as? HTTPURLResponse,
+                    (200...299).contains(response.statusCode) else {
+                        print ("server error")
+                        return
+                }
+                if response.statusCode == 200 {
+                    guard let data = data, let jsonData = try? JSONDecoder().decode(Rubi.self, from: data) else {
+                        print("json変換に失敗しました")
+                        return
+                    }
+                    print(jsonData.converted)
+                    DispatchQueue.main.async {
+                        self.outputText.text = jsonData.converted
+                    }
+                } else {
+                    print("サーバエラー ステータスコード: \(response.statusCode)\n")
+                }
+            }
+            task.resume()
     }
     
 }
